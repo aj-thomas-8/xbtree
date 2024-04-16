b2843a63914cea600c6b49cba5f65792a3dc90d4
Kaitlin McNary <kaitlinmcnary@infra04-wg012.eduroam.wireless.umn.edu>
2024-04-16 14:41:06 -0500

    Added a tag function

--- tagger.py
+++ tagger.py
@@ -1,22 +1,24 @@
 import nltk
 from nltk.corpus import brown
 
-#Train Unigram Tagger on Brown Corpus
-brown_tagged_sents = brown.tagged_sents(categories='news')
+def tag(sent):
+	#Train Unigram Tagger on Brown Corpus
+	brown_tagged_sents = brown.tagged_sents(categories='news')
 
-#some predefined sentence 'sent'
-sent = "I really like cats"
+	#some predefined sentence 'sent'
 
-#setup the unigram tagger
-unigram_tagger = nltk.UnigramTagger(brown_tagged_sents)
+	#setup the unigram tagger
+	unigram_tagger = nltk.UnigramTagger(brown_tagged_sents)
 
-#do all the steps necessary to get the tagger working here (training etc)
-tokens = nltk.word_tokenize(sent)
+	#do all the steps necessary to get the tagger working here (training etc)
+	tokens = nltk.word_tokenize(sent)
 
-#use the tagger on the sentence 'sent'
-tagged_sent = unigram_tagger.tag(tokens)
+	#use the tagger on the sentence 'sent'
+	tagged_sent = unigram_tagger.tag(tokens)
 
-#print 'sent'
-print(tagged_sent)
+	#print 'sent'
+	return tagged_sent
+
+# print(tag("I fought the man"))
 
 
